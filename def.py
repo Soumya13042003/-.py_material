58b064bf68437a7f76482263c2e526d973b3492f
Soumya13042003 <soumyadippaul90@gmail.com>
2026-07-01 01:58:19 +0530

    update file

---
+++ def.py
@@ -0,0 +1,38 @@
+#def even_or_odd(num):
+#    if num%2 == 0:
+#        print("This is even")
+#    else:
+#        print("this is odd")
+#num = float(input("enter any number: "))
+#even_or_odd(num)
+
+#def add(a,b):
+#    return(a+b)
+#result = add(2,4)
+#print(result)
+
+#def greet(name):
+ #   print(f"Hey, {name} are you unemployed.")
+
+#greet("soumya")
+
+#def arguments(*num):
+ #   for number in num:
+  #      print(number)
+
+#arguments(1,2,3,4,5,6,"num")
+#arguments(1,2,3)
+#arguments()
+#arguments(1,2,3,4,5,6)
+
+#def arguments(**kwargs):
+#    for key, value in kwargs.items():
+#        print(f"{key}:{value}")
+
+#arguments(name="Soumyadip", age=23, designation="Unemployed")
+
+
+def number_1(a,b):
+    return a * b, a
+
+print(number_1(2,3))
